5e22920ac3d234fc00ac72eaf5545530f48a0ee3
Robin Szymczak <46560732+robinszym@users.noreply.github.com>
2023-07-14 14:47:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,7 @@ setup(
     maintainer_email="robin@peerdom.org",
     description="Peerdom's Python api wrapper developed by Peerdom",
     long_description=open('README.md').read(),
-    url="https://companyname.com/products/company-package",  # change to github page
-    #    packages=find_packages(where='peerdomclient'),
-    #    package_dir={'': 'peerdomclient'},
+    url="https://github.com/peerdom/peerdom-client-python",
     install_requires=[
         "Requests==2.31.0",
         "setuptools==66.1.1"
